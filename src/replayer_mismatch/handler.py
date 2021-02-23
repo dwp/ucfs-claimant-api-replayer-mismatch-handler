ed07f78256e39ab8239963ea4d85925f0d80dde9
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-23 18:01:47 +0000

    Initial commit, empty lambda that prints out the received event
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

---
+++ src/replayer_mismatch/handler.py
@@ -0,0 +1,111 @@
+import boto3
+import os
+import json
+import argparse
+import sys
+import socket
+import logging
+
+
+def setup_logging(logger_level):
+    the_logger = logging.getLogger()
+    for old_handler in the_logger.handlers:
+        the_logger.removeHandler(old_handler)
+
+    new_handler = logging.StreamHandler(sys.stdout)
+
+    hostname = socket.gethostname()
+
+    json_format = (
+        '{ "timestamp": "%(asctime)s", "log_level": "%(levelname)s", "message": "%(message)s", '
+        f'"environment": "{args.environment}", "application": "{args.application}", '
+        f'"module": "%(module)s", "process": "%(process)s", '
+        f'"thread": "[%(thread)s]", "hostname": "{hostname}" }} '
+    )
+
+    new_handler.setFormatter(logging.Formatter(json_format))
+    the_logger.addHandler(new_handler)
+    new_level = logging.getLevelName(logger_level.upper())
+    the_logger.setLevel(new_level)
+
+    if the_logger.isEnabledFor(logging.DEBUG):
+        boto3.set_stream_logger()
+        the_logger.debug(f'Using boto3", "version": "{boto3.__version__}')
+
+    return the_logger
+
+
+def get_parameters():
+    parser = argparse.ArgumentParser(
+        description="An AWS lambda which receives payload information of replayed mismatch records, "
+                    "and fetches additional information from both databases before recording in DynamoDb."
+    )
+
+    # Parse command line inputs and set defaults
+    parser.add_argument("--aws-profile", default="default")
+    parser.add_argument("--aws-region")
+    parser.add_argument("--environment", default="NOT_SET")
+    parser.add_argument("--application", default="NOT_SET")
+    parser.add_argument("--log-level", default="INFO")
+
+    _args = parser.parse_args()
+
+    # Override arguments with environment variables where set
+    if "AWS_PROFILE" in os.environ:
+        _args.aws_profile = os.environ["AWS_PROFILE"]
+
+    if "AWS_REGION" in os.environ:
+        _args.aws_region = os.environ["AWS_PROFILE"]
+
+    if "ENVIRONMENT" in os.environ:
+        _args.environment = os.environ["ENVIRONMENT"]
+
+    if "APPLICATION" in os.environ:
+        _args.application = os.environ["APPLICATION"]
+
+    if "LOG_LEVEL" in os.environ:
+        _args.log_level = os.environ["LOG_LEVEL"]
+
+    required_args = ["log_level"]
+
+    missing_args = []
+    for required_message_key in required_args:
+        if required_message_key not in _args:
+            missing_args.append(required_message_key)
+    if missing_args:
+        raise argparse.ArgumentError(
+            None,
+            "ArgumentError: The following required arguments are missing: {}".format(
+                ", ".join(missing_args)
+            ),
+        )
+
+    return _args
+
+
+args = None
+logger = None
+
+
+def handler(event, context):
+    global args
+    args = get_parameters()
+    global logger
+    logger = setup_logging(args.log_level)
+
+    logger.info(f"Event: {event}")
+
+
+if __name__ == "__main__":
+    try:
+        args = get_parameters()
+        logger = setup_logging("INFO")
+
+        boto3.setup_default_session(
+            profile_name=args.aws_profile, region_name=args.aws_region
+        )
+        logger.info(os.getcwd())
+        json_content = json.loads(open("../../resources/event.json", "r").read())
+        handler(json_content, None)
+    except Exception as err:
+        logger.error(f'Exception occurred for invocation", "error_message": "{err}')
