57c2a2be254dd42d81dbe3dd84dce651d054cf62
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-03-02 12:21:13 +0000

    Fixup: Format Python code with Black

--- src/replayer_mismatch/handler.py
+++ src/replayer_mismatch/handler.py
@@ -235,7 +235,9 @@ def handler(event, context):
         args.use_ssl,
     )
 
-    ireland_additional_data = query_rds.get_additional_record_data(nino, ireland_connection)
+    ireland_additional_data = query_rds.get_additional_record_data(
+        nino, ireland_connection
+    )
 
     london_sql_password = get_parameter_store_value(
         args.london_rds_parameter, "eu-west-2"
@@ -248,7 +250,9 @@ def handler(event, context):
         args.use_ssl,
     )
 
-    london_additional_data = query_rds.get_additional_record_data(nino, london_connection)
+    london_additional_data = query_rds.get_additional_record_data(
+        nino, london_connection
+    )
 
     ire_len = len(ireland_additional_data)
     ldn_len = len(london_additional_data)
